ba7828f9fe13be000f78f9091df9b5cecf7e21e1
matsa_007 <serg.arbitran@gmail.com>
2024-02-25 23:02:36 +0300

    006: Asset Crypto Hub Added main screen view model and connected to MainCryptoInfoViewController

---
+++ AssetCryptoHub/AssetCryptoHub/CryptoInfo/MainScreen/MainScreenViewModels/MainCryptoInfoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainCryptoInfoViewModel.swift
+//  AssetCryptoHub
+//
+//  Created by Сергей Матвеенко on 25.02.24.
+//
+
+import Foundation
+import Combine
+
+class MainCryptoInfoViewModel: MainCryptoInfoViewModelProtocol {
+    func readyForDisplay() {}
+}
